8abd901e2b03280f091f7dcc851a8c8ac013e297
Nestor novella <importarnorth@gmail.com>
2025-06-13 07:18:04 -0300

    changes

--- src/app/components/navBar/NavBar.tsx
+++ src/app/components/navBar/NavBar.tsx
@@ -1,28 +1,22 @@
 'use client'
-import MainContainer from '@/app/helpers/containers/MainContainer';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { FaBagShopping } from 'react-icons/fa6';
 import { RiAdminFill } from 'react-icons/ri';
-import { BlurModal } from '../modals/modals';
-import useToogleOpenHook from '@/app/admin/hooks/toogleHooks';
+
 
 const NavBar = () => {
 
-    
     const { push } = useRouter()
-    const { close, toogleOpen, status } = useToogleOpenHook()
+
     return (
         <div className='fixed w-full flex h-[70px] justify-between items-centerpx-3 md:px-20 xl:px-36 2xl:px-48 z-[100] backdrop-blur-3xl'>
             <div onClick={() => { push('/') }} className='relative border cursor-pointer rounded p-2 bg-pink-500 text-white font-bold flex items-center justify-center'>
-                <BlurModal close={close} status={status} >
-                    <h2>modal abierto</h2>
-                </BlurModal>
                 <h2 className='text-2xl '>LOGO</h2>
             </div>
 
             <div className='p-2 flex justify-center items-center cursor-pointer'>
-                <RiAdminFill onClick={() => { toogleOpen() }} className='size-10 text-green-500' />
+                <RiAdminFill onClick={() => { push('/admin') }} className='size-10 text-green-500' />
             </div>
 
             <div className='p-2 flex justify-center items-center cursor-pointer'>
